ee5a0ecc70d94ff95656a3917251d1612cc983d8
kuldeep1403 <kuldeepsingh.14rewa@gmail.com>
2023-04-11 11:17:45 +0530

    assignment 3

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -10,6 +10,14 @@
     "Paula",
     "Laura",
     "Jim",
+    "Nick",
+    "Ethan",
+    "Ava",
+    "Jackson",
+    "Olivia",
+    "Caleb",
+    "Sophia",
+    "Liam",
   ];
 
   for (var i = 0; i < names.length; i++) {
